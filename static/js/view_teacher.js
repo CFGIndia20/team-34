329e0beab9e07c032ec42de6f1b00d1a0097fca1
desaiayu <desaiayu@gmail.com>
2020-07-26 05:17:58 +0530

    Admin allottment buttons

--- static/js/view_teacher.js
+++ static/js/view_teacher.js
@@ -1,3 +1,5 @@
-var colors = ['#ff0000', '#00ff00', '#0000ff'];
-var random_color = colors[Math.floor(Math.random() * colors.length)];
-document.getElementById('batch').style.background-color = random_color;
+function colorBadge(this) {
+    var colors = ['#ff0000', '#00ff00', '#0000ff'];
+    var random_color = colors[Math.floor(Math.random() * colors.length)];
+    document.getElementById(this).style.color = random_color;
+}
