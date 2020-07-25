0420384fdd397624a87c801b97a8d05fc8dd0f17
manandoshi1607 <manan.doshi@spit.ac.in>
2020-07-25 22:02:24 +0530

    Updated: DB Migration SQL to MongoDB

--- app.py
+++ app.py
@@ -1,70 +1,84 @@
-from flask import Flask, jsonify, request, session, flash, render_template
-from register import upload_user_data, upload_slot_data, upload_entrance_test_data
-from db import students, db
-from datetime import timedelta
+from flask import Flask, jsonify, request, session, redirect, render_template
+import repo
+import bcrypt
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "geeksforsocialchange"
 app.debug = True
-
-
-app.config["SECRET_KEY"] = "fmq049kbjmajf734nvadinhfg"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user.sqlite3"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(hours=1)
-
-
-@app.route("/welcome", methods=["GET"])
-def welcome():
-    return jsonify({"data": "Success"})
-
 
 @app.route("/", methods=["GET"])
 def index():
     return render_template('index.html')
 
-@app.route("/register/userdata", methods=["GET","POST"])
-def register_user_data():
-    if request.method == "POST":
+@app.route("/register/userdata", methods=["GET", "POST"])
+def register_userdata():
+    if request.method == 'GET':
+        return render_template('register.html')
+    elif request.method == 'POST':
+        print("Into register userdata")
+        form = {}
         print(request.form)
-        # name = request.form["name"]
-        # email = request.form["email"]
-        # password = request.form["password"]
-        # dob = request.form["dob"]
-        # aadhar_no = request.form["aadhar_no"]
-        # address = request.form["address"]
-        # entrance_score = request.form["entrance_score"]
-        # is_enrolled = request.form["is_enrolled"]
-        # slot_preference_1 = request.form["slot_preference_1"]
-        # slot_preference_2 = request.form["slot_preference_2"]
-        # slot_preference_3 = request.form["slot_preference_3"]
-        # slot_preference_4 = request.form["slot_preference_4"]
-        # batch_id = request.form["batch_id"]
-        # upload_user_data(name, email, password, dob, aadhar_no, address, entrance_score, is_enrolled, slot_preference_1, slot_preference_2, slot_preference_3, slot_preference_4, batch_id)
+
+        form["name"] = request.form["name"]
+        form["email"] = request.form["email"]
+        form["password"] = request.form["pwd"]
+        form["dob"] = request.form["dob"]
+        form["aadhar_no"] = request.form["aadhar"]
+        form["address"] = request.form["address"]
+
+        salt = bcrypt.gensalt()
+        hashed = bcrypt.hashpw(form["password"].encode(), salt)
+        form["password"] = hashed
+
+        ob = repo.OhlcRepo()
+        print(ob)
+        ob.insert_record_one(collection_name="students",insert_doc=form)
+
         session.permanent = True
-        session["email"] = "email"
-        return render_template("baseline_quiz.html")
-    else:
-        return render_template("register.html")
+        session["email"] = form["email"]
 
+        return redirect("/register/entrance_test")
 
-@app.route("/register/entrance_test_data", methods=["GET", "POST"])
-def register_entrance_test_data():
-    if request.method == "POST":
+@app.route("/register/entrance_test", methods=["GET", "POST"])
+def register_entrance():
+    if request.method == 'GET':
+        return render_template('baseline_quiz.html')
+    elif request.method == 'POST':
+        form = {}
+        if request.args:
+            form["score"] = request.args.get("score")
+            form["email"] = session["email"]
+            
+            ob = repo.OhlcRepo()
+            results = ob.find_record_with_projection(collection_name="students", query={"email": form["email"]}, projection={"email": 1})
+            if len(results) != 1:
+                return jsonify({"error": "Student not found"})
+            ob.update_one(collection_name="students",query={"email": form["email"]},insert_doc={"$set":form})
+
+        return redirect("/register/slots_preference")
+
+@app.route("/register/slots_preference", methods=["GET", "POST"])
+def register_slots():
+    if request.method == 'GET':
+        return render_template('slots.html')
+    elif request.method == 'POST':
+        form = {}
         print(request.form)
-        return render_template("slots.html")
-    else:
-        return render_template("baseline_quiz.html")
+        print("\n\n")
+        
+        form["p1"] = request.form["p1"]
+        form["p2"] = request.form["p2"]
+        form["p3"] = request.form["p3"]
+        form["p4"] = request.form["p4"]
+        form["email"] = session["email"]
+        
+        ob = repo.OhlcRepo()
+        results = ob.find_record_with_projection(collection_name="students", query={"email": form["email"]}, projection={"email": 1})
+        if len(results) != 1:
+            return jsonify({"error": "Student not found"})
+        ob.update_one(collection_name="students",query={"email": form["email"]},insert_doc={"$set":form})
 
-@app.route("/register/slots_preference", methods=["GET","POST"])
-def register_slot_data():
-    if request.method == "POST":
-        print(request.form)
-        return "Application Submitted"
-    else:
-        return render_template("slots.html")
-
-db.init_app(app)
-db.create_all(app=app)
+        return jsonify({"data": "Success"})
 
 if __name__ == '__main__':
     app.run()
