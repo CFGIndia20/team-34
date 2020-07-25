d4d204d4ce84dcd2f00599fb18a104cb96b982c0
desaiayu <desaiayu@gmail.com>
2020-07-25 19:09:38 +0530

    Registering User Details

---
+++ static/js/register.js
@@ -0,0 +1,9 @@
+function onReset() {
+    document.getElementById("name").value = "";
+    document.getElementById("address").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("dob").value = "";
+    document.getElementById("aadhar").value = "";
+    document.getElementById("pwd").value = "";
+    document.getElementById("cpwd").value = "";
+}
