2b6e3df6439d25deb0c454df04a2bccd39b3cd58
desaiayu <desaiayu@gmail.com>
2020-07-26 05:24:05 +0530

    View teachers page modified: shows table

--- static/js/view_teacher.js
+++
@@ -1,5 +0,0 @@
-function colorBadge(this) {
-    var colors = ['#ff0000', '#00ff00', '#0000ff'];
-    var random_color = colors[Math.floor(Math.random() * colors.length)];
-    document.getElementById(this).style.color = random_color;
-}
